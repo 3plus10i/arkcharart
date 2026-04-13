40ffd3734a4de13d8f1c544f5a8c34d6293392e0
srs <ctrlyjy@qq.com>
2026-04-13 19:18:37 +0800

    fix(config): 修复静态资源目录配置

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',
+  publicDir: 'assets',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
+    assetsDir: 'static',
     emptyOutDir: true,
   },
   server: {
