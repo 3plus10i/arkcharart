1468ee0d5ef4894ddb70891e2bca29b0ca05e416
srs <ctrlyjy@qq.com>
2026-04-13 19:26:38 +0800

    fix: 更新资源文件位置，修复vite配置

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',
-  publicDir: 'assets',
   build: {
     outDir: 'dist',
     assetsDir: 'static',
